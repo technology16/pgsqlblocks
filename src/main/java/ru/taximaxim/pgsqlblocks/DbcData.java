e3f2893d96eb8cdc6955ce988df593f78a54cdfa
Петров Иван <petrov_im@taximaxim.local>
2015-10-09 10:29:36 +0300

    ошибки сонара

--- src/main/java/ru/taximaxim/pgsqlblocks/DbcData.java
+++ src/main/java/ru/taximaxim/pgsqlblocks/DbcData.java
@@ -1,9 +1,7 @@
 package ru.taximaxim.pgsqlblocks;
 
 import java.io.BufferedReader;
-import java.io.File;
 import java.io.FileNotFoundException;
-import java.io.FileReader;
 import java.io.IOException;
 import java.nio.charset.StandardCharsets;
 import java.nio.file.Files;
--- src/main/java/ru/taximaxim/pgsqlblocks/Provider.java
+++ src/main/java/ru/taximaxim/pgsqlblocks/Provider.java
@@ -2,8 +2,6 @@ package ru.taximaxim.pgsqlblocks;
 
 import java.io.BufferedReader;
 import java.io.IOException;
-import java.io.InputStream;
-import java.io.InputStreamReader;
 import java.nio.charset.StandardCharsets;
 import java.nio.file.Files;
 import java.nio.file.Paths;
