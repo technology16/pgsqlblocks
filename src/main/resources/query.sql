e32234f32faf10b4946fbc048aece858de1d1662
Anton Ryabinin <ryabinin_av@taximaxim.ru>
2016-09-07 16:06:11 +0300

    Change query - extract joined table to WITH (#11815)

--- src/main/resources/query.sql
+++ src/main/resources/query.sql
@@ -1,3 +1,24 @@
+WITH bl AS (
+    SELECT blocking_locks.pid as pid, 
+    blocked_locks.pid as blocked_pid
+    FROM pg_catalog.pg_locks blocked_locks
+    JOIN pg_catalog.pg_stat_activity blocked_activity 
+        ON blocked_activity.pid = blocked_locks.pid
+    JOIN pg_catalog.pg_locks blocking_locks 
+        ON blocking_locks.locktype = blocked_locks.locktype
+        AND blocking_locks.DATABASE IS NOT DISTINCT FROM blocked_locks.DATABASE
+        AND blocking_locks.relation IS NOT DISTINCT FROM blocked_locks.relation
+        AND blocking_locks.page IS NOT DISTINCT FROM blocked_locks.page
+        AND blocking_locks.tuple IS NOT DISTINCT FROM blocked_locks.tuple
+        AND blocking_locks.virtualxid IS NOT DISTINCT FROM blocked_locks.virtualxid
+        AND blocking_locks.transactionid IS NOT DISTINCT FROM blocked_locks.transactionid
+        AND blocking_locks.classid IS NOT DISTINCT FROM blocked_locks.classid
+        AND blocking_locks.objid IS NOT DISTINCT FROM blocked_locks.objid
+        AND blocking_locks.objsubid IS NOT DISTINCT FROM blocked_locks.objsubid
+        AND blocking_locks.pid != blocked_locks.pid
+    JOIN pg_catalog.pg_stat_activity sa ON sa.pid = blocking_locks.pid
+    WHERE NOT blocked_locks.granted
+)
 SELECT 
     /*---------------------*/
     p.pid AS pid, 
@@ -41,26 +62,6 @@ SELECT
     END AS slowquery 
     /*---------------------*/
     FROM pg_stat_activity p 
-    LEFT JOIN (
-                    SELECT blocking_locks.pid as pid, 
-                    blocked_locks.pid as blocked_pid
-                    FROM pg_catalog.pg_locks blocked_locks
-                    JOIN pg_catalog.pg_stat_activity blocked_activity 
-                        ON blocked_activity.pid = blocked_locks.pid
-                    JOIN pg_catalog.pg_locks blocking_locks 
-                        ON blocking_locks.locktype = blocked_locks.locktype
-                        AND blocking_locks.DATABASE IS NOT DISTINCT FROM blocked_locks.DATABASE
-                        AND blocking_locks.relation IS NOT DISTINCT FROM blocked_locks.relation
-                        AND blocking_locks.page IS NOT DISTINCT FROM blocked_locks.page
-                        AND blocking_locks.tuple IS NOT DISTINCT FROM blocked_locks.tuple
-                        AND blocking_locks.virtualxid IS NOT DISTINCT FROM blocked_locks.virtualxid
-                        AND blocking_locks.transactionid IS NOT DISTINCT FROM blocked_locks.transactionid
-                        AND blocking_locks.classid IS NOT DISTINCT FROM blocked_locks.classid
-                        AND blocking_locks.objid IS NOT DISTINCT FROM blocked_locks.objid
-                        AND blocking_locks.objsubid IS NOT DISTINCT FROM blocked_locks.objsubid
-                        AND blocking_locks.pid != blocked_locks.pid
-                    JOIN pg_catalog.pg_stat_activity sa ON sa.pid = blocking_locks.pid
-                    WHERE NOT blocked_locks.granted
-    ) as bl
+    LEFT JOIN bl
     ON bl.blocked_pid = p.pid
     ORDER BY 1 ASC
