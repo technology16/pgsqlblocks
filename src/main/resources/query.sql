82cef02cd49073b217e3d3fb349a97816dcfafb5
Шайдуллин Ильдар <shaydullin_iz@taximaxim.ru>
2016-09-08 17:23:38 +0300

    Update query.sql: without idle processes

--- src/main/resources/query.sql
+++ src/main/resources/query.sql
@@ -64,4 +64,7 @@ SELECT
     FROM pg_stat_activity p 
     LEFT JOIN bl
     ON bl.blocked_pid = p.pid
+    WHERE p.pid IN (SELECT pid
+			FROM pg_stat_activity p1
+			WHERE p1.state != 'idle' OR p1.state IS NULL)
     ORDER BY 1 ASC
