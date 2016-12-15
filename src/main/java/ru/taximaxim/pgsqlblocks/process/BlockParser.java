feb3b7b8997bf08ae485a4e36b6a15f2a9c8b652
Anton Ryabinin <ryabinin_av@taximaxim.ru>
2016-12-15 18:40:08 +0300

    fix sonar

--- src/main/java/ru/taximaxim/pgsqlblocks/process/BlockParser.java
+++ src/main/java/ru/taximaxim/pgsqlblocks/process/BlockParser.java
@@ -3,14 +3,12 @@ package ru.taximaxim.pgsqlblocks.process;
 import org.apache.log4j.Logger;
 import org.w3c.dom.Document;
 import org.w3c.dom.Element;
-import sun.reflect.generics.reflectiveObjects.NotImplementedException;
 
 public class BlockParser {
-    private static final String CHILDREN = "children";
     
-    private static final Logger LOG = Logger.getLogger(BlockParser.class);
+    private static final Logger LOG = Logger.getLogger(BlockParser.class); // NOSONAR
 
     public Element createBlockElement(Document doc, Block block) {
-        throw new NotImplementedException();
+        throw new RuntimeException("Not implemented");
     }
 }
