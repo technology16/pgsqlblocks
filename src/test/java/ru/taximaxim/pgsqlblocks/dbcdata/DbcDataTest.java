62ab5a95af6d6a544fd45d3c40efccd45248cad0
Anton Ryabinin <ryabinin_av@taximaxim.ru>
2017-01-13 17:02:00 +0300

    Simplify code, remove duplicate lines

--- src/test/java/ru/taximaxim/pgsqlblocks/dbcdata/DbcDataTest.java
+++ src/test/java/ru/taximaxim/pgsqlblocks/dbcdata/DbcDataTest.java
@@ -18,40 +18,70 @@ import static ru.taximaxim.pgsqlblocks.TEST.*;
 public class DbcDataTest {
     private static DbcData testDbc;
     private static final Logger LOG = Logger.getLogger(DbcDataTest.class);
-    private static Map<Integer,Connection> connectionList = new HashMap<>();
+    private static List<ConnInfo> connectionList = new ArrayList<>();
 
     @BeforeClass
     public static void initialize() throws IOException {
         testDbc = new DbcData("TestDbc", REMOTE_HOST,  REMOTE_PORT, REMOTE_DB,
                 REMOTE_USERNAME,  REMOTE_PASSWORD,  true);
+        testDbc.connect();
+    }
+
+    @AfterClass
+    public static void terminate() throws SQLException {
+        testDbc.disconnect();
     }
 
     @Before
     public void beforeTest() throws IOException, SQLException {
-        if (!testDbc.isConnected()) {
-            testDbc.connect();
-        }
+        Connection conn1 = getNewConnector();
+        connectionList.add(new ConnInfo(getPid(conn1), conn1));
+
+        Connection conn2 = getNewConnector();
+        connectionList.add(new ConnInfo(getPid(conn2), conn2));
+
+        Connection conn3 = getNewConnector();
+        connectionList.add(new ConnInfo(getPid(conn3), conn3));
+
         /* prepare db */
         testDbc.getConnection().prepareStatement(ProcessTreeBuilder.loadQuery(TESTING_DUMP_SQL)).execute();
         /* create rule */
         testDbc.getConnection().prepareStatement(ProcessTreeBuilder.loadQuery(CREATE_RULE_SQL)).execute();
     }
 
+    @After
+    public void executionConns() throws SQLException {
+        List<ConnInfo> newList = new ArrayList<>();
+        for(ConnInfo connInfo : connectionList) {
+            try (PreparedStatement termPs = testDbc.getConnection().prepareStatement(PG_TERMINATE_BACKEND)) {
+                termPs.setInt(1, connInfo.getPid());
+                LOG.info("Prepared query:" + termPs);
+                ResultSet result = termPs.executeQuery();
+                if (result.next()) {
+                    boolean terminatedSuccesed = result.getBoolean(TERMINATED_SUCCESED);
+                    LOG.info("Terminating the process pid:" + connInfo.getPid() + " is succeed:" + terminatedSuccesed);
+                    
+                    if (terminatedSuccesed) {
+                        LOG.info("Process terminated:" + connInfo.getPid());
+                        connInfo.getConnection().close();
+                    } else {
+                        LOG.info("Process cannot be terminated:" + connInfo.getPid());
+                        newList.add(connInfo);
+                    }
+                }
+                assert !result.isAfterLast();
+            } catch (Exception e) {
+                LOG.error("Error on prepared statement" + e.getMessage());
+            }
+        }
+        connectionList = newList;
+    }
+
     @Test
     public void testMultipleLocks() throws IOException, SQLException, InterruptedException {
-        Connection conn1 = getNewConnector();
-        int conn1Pid = getPid(conn1);
-        connectionList.put(conn1Pid, conn1);
-        Connection conn2 = getNewConnector();
-        int conn2Pid = getPid(conn2);
-        connectionList.put(conn2Pid, conn2);
-        Connection conn3 = getNewConnector();
-        int conn3Pid = getPid(conn3);
-        connectionList.put(conn3Pid, conn3);
-
-        PreparedStatement statement1 = conn1.prepareStatement(ProcessTreeBuilder.loadQuery(TEST_DROP_RULE_SQL));
-        PreparedStatement statement2 = conn2.prepareStatement(ProcessTreeBuilder.loadQuery(TEST_SELECT_1000_SQL));
-        PreparedStatement statement3 = conn3.prepareStatement(ProcessTreeBuilder.loadQuery(TEST_SELECT_1000_SQL));
+        PreparedStatement statement1 = connectionList.get(0).getConnection().prepareStatement(ProcessTreeBuilder.loadQuery(TEST_DROP_RULE_SQL));
+        PreparedStatement statement2 = connectionList.get(1).getConnection().prepareStatement(ProcessTreeBuilder.loadQuery(TEST_SELECT_1000_SQL));
+        PreparedStatement statement3 = connectionList.get(2).getConnection().prepareStatement(ProcessTreeBuilder.loadQuery(TEST_SELECT_1000_SQL));
 
         /* Execute in the following order 2, 3, 1 */
         Thread thread2 = getNewThread(statement2);
@@ -70,9 +100,9 @@ public class DbcDataTest {
                 flatMap(l -> l.getChildren().stream()).
                 collect(Collectors.toList());
 
-        Optional <Process> proc1 = allGrandChild.stream().filter(x -> x.getPid() == conn1Pid).findFirst();
-        Optional <Process> proc2 = rootProcess.getChildren().stream().filter(x -> x.getPid() == conn2Pid).findFirst();
-        Optional <Process> proc3 = rootProcess.getChildren().stream().filter(x -> x.getPid() == conn3Pid).findFirst();
+        Optional <Process> proc1 = allGrandChild.stream().filter(x -> x.getPid() == connectionList.get(0).getPid()).findFirst();
+        Optional <Process> proc2 = rootProcess.getChildren().stream().filter(x -> x.getPid() == connectionList.get(1).getPid()).findFirst();
+        Optional <Process> proc3 = rootProcess.getChildren().stream().filter(x -> x.getPid() == connectionList.get(2).getPid()).findFirst();
 
         assertTrue(proc1.isPresent());
         assertTrue(proc2.isPresent());
@@ -90,21 +120,12 @@ public class DbcDataTest {
         thread1.interrupt();
         thread3.interrupt();
         thread2.interrupt();
-
-        executionConns();
     }
 
     @Test
     public void testReproWaitingLocks() throws IOException, SQLException, InterruptedException {
-        Connection conn1 = getNewConnector();
-        int conn1Pid = getPid(conn1);
-        connectionList.put(conn1Pid, conn1);
-        Connection conn2 = getNewConnector();
-        int conn2Pid = getPid(conn2);
-        connectionList.put(conn2Pid, conn2);
-
-        PreparedStatement statement1 = conn1.prepareStatement(ProcessTreeBuilder.loadQuery(TEST_SELECT_SLEEP_SQL));
-        PreparedStatement statement2 = conn2.prepareStatement(ProcessTreeBuilder.loadQuery(TEST_CREATE_INDEX_SQL));
+        PreparedStatement statement1 = connectionList.get(0).getConnection().prepareStatement(ProcessTreeBuilder.loadQuery(TEST_SELECT_SLEEP_SQL));
+        PreparedStatement statement2 = connectionList.get(1).getConnection().prepareStatement(ProcessTreeBuilder.loadQuery(TEST_CREATE_INDEX_SQL));
 
         /* Execute in the following order 1, 2 */
         Thread thread1 = getNewThread(statement1);
@@ -119,8 +140,8 @@ public class DbcDataTest {
                 flatMap(l -> l.getChildren().stream()).
                 collect(Collectors.toList());
 
-        Optional <Process> proc1 = rootProcess.getChildren().stream().filter(x -> x.getPid() == conn1Pid).findFirst();
-        Optional <Process> proc2 = allGrandChild.stream().filter(x -> x.getPid() == conn2Pid).findFirst();
+        Optional <Process> proc1 = rootProcess.getChildren().stream().filter(x -> x.getPid() == connectionList.get(0).getPid()).findFirst();
+        Optional <Process> proc2 = allGrandChild.stream().filter(x -> x.getPid() == connectionList.get(1).getPid()).findFirst();
 
         assertTrue(proc1.isPresent());
         assertTrue(proc2.isPresent());
@@ -133,8 +154,6 @@ public class DbcDataTest {
 
         thread2.interrupt();
         thread1.interrupt();
-
-        executionConns();
     }
 
     @Test
@@ -142,17 +161,10 @@ public class DbcDataTest {
         /* create rule */
         testDbc.getConnection().prepareStatement(ProcessTreeBuilder.loadQuery(CREATE_RULE_SQL)).execute();
 
-        Connection conn1 = getNewConnector();
-        int conn1Pid = getPid(conn1);
-        connectionList.put(conn1Pid, conn1);
-        Connection conn2 = getNewConnector();
-        int conn2Pid = getPid(conn2);
-        connectionList.put(conn2Pid, conn2);
+        assertTrue(connectionList.get(0).getPid() < connectionList.get(1).getPid());
 
-        assertTrue(conn1Pid < conn2Pid);
-
-        PreparedStatement statement1 = conn1.prepareStatement(ProcessTreeBuilder.loadQuery(TEST_DROP_RULE_SQL));
-        PreparedStatement statement2 = conn2.prepareStatement(ProcessTreeBuilder.loadQuery(TEST_SELECT_1000_SQL));
+        PreparedStatement statement1 = connectionList.get(0).getConnection().prepareStatement(ProcessTreeBuilder.loadQuery(TEST_DROP_RULE_SQL));
+        PreparedStatement statement2 = connectionList.get(1).getConnection().prepareStatement(ProcessTreeBuilder.loadQuery(TEST_SELECT_1000_SQL));
 
         /* Execute in the following order 2, 1 */
         Thread thread2 = getNewThread(statement2);
@@ -168,8 +180,8 @@ public class DbcDataTest {
                 flatMap(l -> l.getChildren().stream()).
                 collect(Collectors.toList());
 
-        Optional <Process> proc1 = allGrandChild.stream().filter(x -> x.getPid() == conn1Pid).findFirst();
-        Optional <Process> proc2 = rootProcess.getChildren().stream().filter(x -> x.getPid() == conn2Pid).findFirst();
+        Optional <Process> proc1 = allGrandChild.stream().filter(x -> x.getPid() == connectionList.get(0).getPid()).findFirst();
+        Optional <Process> proc2 = rootProcess.getChildren().stream().filter(x -> x.getPid() == connectionList.get(1).getPid()).findFirst();
 
         assertTrue(proc1.isPresent());
         assertTrue(proc2.isPresent());
@@ -182,9 +194,6 @@ public class DbcDataTest {
 
         thread1.interrupt();
         thread2.interrupt();
-        executionConns();
-        conn1.close();
-        conn2.close();
     }
 
     //TODO: remove ignore annotation and fix test after solving the issue #12290
@@ -194,19 +203,9 @@ public class DbcDataTest {
         /* create rule */
         testDbc.getConnection().prepareStatement(ProcessTreeBuilder.loadQuery(CREATE_RULE_SQL)).execute();
 
-        Connection conn1 = getNewConnector();
-        int conn1Pid = getPid(conn1);
-        connectionList.put(conn1Pid, conn1);
-        Connection conn2 = getNewConnector();
-        int conn2Pid = getPid(conn2);
-        connectionList.put(conn2Pid, conn2);
-        Connection conn3 = getNewConnector();
-        int conn3Pid = getPid(conn3);
-        connectionList.put(conn3Pid, conn3);
-
-        PreparedStatement statement1 = conn1.prepareStatement(ProcessTreeBuilder.loadQuery(TEST_SELECT_1000_SQL));
-        PreparedStatement statement2 = conn2.prepareStatement(ProcessTreeBuilder.loadQuery(TEST_DROP_RULE_SQL));
-        PreparedStatement statement3 = conn3.prepareStatement(ProcessTreeBuilder.loadQuery(TEST_SELECT_1000_SQL));
+        PreparedStatement statement1 = connectionList.get(0).getConnection().prepareStatement(ProcessTreeBuilder.loadQuery(TEST_SELECT_1000_SQL));
+        PreparedStatement statement2 = connectionList.get(1).getConnection().prepareStatement(ProcessTreeBuilder.loadQuery(TEST_DROP_RULE_SQL));
+        PreparedStatement statement3 = connectionList.get(2).getConnection().prepareStatement(ProcessTreeBuilder.loadQuery(TEST_SELECT_1000_SQL));
 
         /* Execute in the following order 1, 2, 3 */
         Thread thread1 = getNewThread(statement1);
@@ -225,9 +224,9 @@ public class DbcDataTest {
                 flatMap(l -> l.getChildren().stream()).
                 collect(Collectors.toList());
 
-        Optional <Process> proc1 = rootProcess.getChildren().stream().filter(x -> x.getPid() == conn1Pid).findFirst();
-        Optional <Process> proc2 = allGrandChild.stream().filter(x -> x.getPid() == conn2Pid).findFirst();
-        Optional <Process> proc3 = allGrandChild.stream().filter(x -> x.getPid() == conn3Pid).findFirst();
+        Optional <Process> proc1 = rootProcess.getChildren().stream().filter(x -> x.getPid() == connectionList.get(0).getPid()).findFirst();
+        Optional <Process> proc2 = allGrandChild.stream().filter(x -> x.getPid() == connectionList.get(1).getPid()).findFirst();
+        Optional <Process> proc3 = allGrandChild.stream().filter(x -> x.getPid() == connectionList.get(2).getPid()).findFirst();
 
 
         assertTrue(proc1.isPresent());
@@ -248,14 +247,6 @@ public class DbcDataTest {
         thread3.interrupt();
         thread2.interrupt();
         thread1.interrupt();
-
-        executionConns();
-    }
-
-    @AfterClass
-    public static void terminate() throws SQLException {
-        executionConns();
-        testDbc.disconnect();
     }
 
     private Connection getNewConnector() throws SQLException {
@@ -286,29 +277,22 @@ public class DbcDataTest {
         }
         return pid;
     }
+}
 
-    private static void executionConns() throws SQLException {
-        Map<Integer, Connection> newList = new HashMap<>();
-        for(Map.Entry<Integer, Connection> entrySet : connectionList.entrySet()) {
-            try (PreparedStatement termPs = testDbc.getConnection().prepareStatement(PG_TERMINATE_BACKEND)) {
-                termPs.setInt(1, entrySet.getKey());
-                LOG.info("Prepared query:" + termPs);
-                ResultSet result = termPs.executeQuery();
-                if (result.next()) {
-                    boolean terminatedSuccesed = result.getBoolean(TERMINATED_SUCCESED);
-                    LOG.info("Terminating the process pid:" + entrySet.getKey() + " is succeed:" + terminatedSuccesed);
-                    if (terminatedSuccesed) {
-                        LOG.info("Process terminated:" + entrySet.getKey());
-                        entrySet.getValue().close();
-                    } else {
-                        LOG.info("Process cannot be terminated:" + entrySet.getKey());
-                        newList.put(entrySet.getKey(), entrySet.getValue());
-                    }
-                }
-            } catch (Exception e) {
-                LOG.error("Error on prepared statement" + e.getMessage());
-            }
-        }
-        connectionList = newList;
+class ConnInfo {
+    private final int pid;
+    private final Connection connection;
+
+    public ConnInfo(int pid, Connection connection) {
+        this.pid = pid;
+        this.connection = connection;
+    }
+
+    public int getPid() {
+        return pid;
+    }
+
+    public Connection getConnection() {
+        return connection;
     }
 }
