68772a82c7819d2bf782e8b2224af33b59ff0d7d
Anton Ryabinin <ryabinin_av@taximaxim.ru>
2017-01-20 17:53:34 +0300

    version 1.3.4

--- src/main/java/ru/taximaxim/pgsqlblocks/utils/Settings.java
+++ src/main/java/ru/taximaxim/pgsqlblocks/utils/Settings.java
@@ -10,7 +10,6 @@ import java.util.stream.Collectors;
 
 import org.apache.log4j.Logger;
 import ru.taximaxim.pgsqlblocks.SortColumn;
-import ru.taximaxim.pgsqlblocks.SortColumn;
 
 /**
  * Класс для работы с настройка пользователя
