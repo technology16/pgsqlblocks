30be63749d61b388f0f1e431c27469147e9f0c98
Шайдуллин Ильдар <shaydullin_iz@taximaxim.ru>
2017-03-09 13:16:01 +0300

    Add license

--- src/main/java/ru/taximaxim/pgsqlblocks/utils/Settings.java
+++ src/main/java/ru/taximaxim/pgsqlblocks/utils/Settings.java
@@ -1,15 +1,32 @@
-package ru.taximaxim.pgsqlblocks.utils;
+/*
+ * Copyright 2017 "Technology" LLC
+ *
+ * Licensed to the Apache Software Foundation (ASF) under one or more
+ * contributor license agreements.  See the NOTICE file distributed with
+ * this work for additional information regarding copyright ownership.
+ * The ASF licenses this file to You under the Apache License, Version 2.0
+ * (the "License"); you may not use this file except in compliance with
+ * the License.  You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
 
-import java.io.File;
-import java.io.FileInputStream;
-import java.io.FileNotFoundException;
-import java.io.FileOutputStream;
-import java.io.IOException;
-import java.util.*;
-import java.util.stream.Collectors;
+package ru.taximaxim.pgsqlblocks.utils;
 
 import org.apache.log4j.Logger;
 import ru.taximaxim.pgsqlblocks.SortColumn;
+
+import java.io.*;
+import java.util.Arrays;
+import java.util.Properties;
+import java.util.Set;
+import java.util.stream.Collectors;
 
 /**
  * Класс для работы с настройка пользователя
