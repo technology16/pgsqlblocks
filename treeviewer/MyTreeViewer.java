c71d0d6fd881470a51cf41e9454c34f7c516e234
Сагель Наталья <sagel_no@taximaxim.local>
2018-08-22 15:47:55 +0300

    fix show and hide of filterview

--- treeviewer/MyTreeViewer.java
+++ treeviewer/MyTreeViewer.java
@@ -7,12 +7,15 @@ import org.eclipse.swt.widgets.Composite;
 import org.eclipse.swt.widgets.ToolBar;
 import org.eclipse.swt.widgets.ToolItem;
 import ru.taximaxim.treeviewer.dialog.ColumnConfigDialog;
+import ru.taximaxim.treeviewer.filter.MyTreeViewerFilter;
 import ru.taximaxim.treeviewer.listeners.DataUpdateListener;
+import ru.taximaxim.treeviewer.models.IColumn;
 import ru.taximaxim.treeviewer.models.MyTreeViewerDataSource;
 import ru.taximaxim.treeviewer.tree.MyTreeViewerTable;
 import ru.taximaxim.treeviewer.utils.ImageUtils;
 import ru.taximaxim.treeviewer.utils.Images;
 
+import java.util.List;
 import java.util.ResourceBundle;
 
 /**
@@ -33,6 +36,7 @@ public class MyTreeViewer extends Composite {
     private ToolItem filterToolItem;
     private MyTreeViewerTable tree;
     private MyTreeViewerDataSource dataSource;
+    private MyTreeViewerFilter filter;
     private DataUpdateListener dataUpdateListener;
 
 
@@ -65,12 +69,13 @@ public class MyTreeViewer extends Composite {
 
     private void createContent() {
         createToolItems();
+        filter = new MyTreeViewerFilter(this, SWT.TOP);
+        filter.hide();
         tree = new MyTreeViewerTable(MyTreeViewer.this, SWT.FILL | SWT.BORDER | SWT.V_SCROLL | SWT.H_SCROLL | SWT.FULL_SELECTION | SWT.MULTI);
     }
 
     private void createToolItems() {
         toolBar = new ToolBar(this, SWT.HORIZONTAL);
-        // TODO: 20.08.18 Listeners!!!
         updateToolItem = new ToolItem(toolBar, SWT.PUSH);
         updateToolItem.setImage(ImageUtils.getImage(Images.UPDATE));
         //updateToolItem.setToolTipText(Images.UPDATE.getDescription(resourceBundle));
@@ -78,6 +83,7 @@ public class MyTreeViewer extends Composite {
 
         filterToolItem = new ToolItem(toolBar, SWT.PUSH);
         filterToolItem.setImage(ImageUtils.getImage(Images.FILTER));
+        filterToolItem.addListener(SWT.Selection, event -> openFilter());
         //filterToolItem.setToolTipText(Images.TABLE.getDescription(resourceBundle));
 
         configColumnToolItem = new ToolItem(toolBar, SWT.PUSH);
@@ -86,8 +92,13 @@ public class MyTreeViewer extends Composite {
         configColumnToolItem.addListener(SWT.Selection, event -> openConfigColumnDialog());
     }
 
+    private void openFilter() {
+        if (filter.isVisible()) {
+            filter.hide();
+        }else filter.show();
+    }
+
     private void updateTreeViewerData() {
-//        tree.setInput(dataSource.updateDataInTree());
         if (dataUpdateListener != null) {
             dataUpdateListener.needUpdateData();
         }
@@ -98,5 +109,9 @@ public class MyTreeViewer extends Composite {
         ColumnConfigDialog dialog = new ColumnConfigDialog(resourceBundle, tree, this.getShell());
         dialog.open();
 
+    }
+
+    public void setFilters(List<IColumn> filters) {
+        filter.setFilterList(filters);
     }
 }
