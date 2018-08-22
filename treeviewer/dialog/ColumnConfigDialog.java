697007f076928ff371d5bc45f4cfd0d11a6be9d2
Сагель Наталья <sagel_no@taximaxim.local>
2018-08-22 11:19:16 +0300

    column dialog with showing/hiding column

---
+++ treeviewer/dialog/ColumnConfigDialog.java
@@ -0,0 +1,76 @@
+package ru.taximaxim.treeviewer.dialog;
+
+import org.eclipse.jface.dialogs.Dialog;
+import org.eclipse.swt.SWT;
+import org.eclipse.swt.layout.GridLayout;
+import org.eclipse.swt.widgets.Button;
+import org.eclipse.swt.widgets.Composite;
+import org.eclipse.swt.widgets.Control;
+import org.eclipse.swt.widgets.Shell;
+import ru.taximaxim.treeviewer.models.IColumn;
+import ru.taximaxim.treeviewer.models.MyTreeViewerDataSource;
+import ru.taximaxim.treeviewer.tree.MyTreeViewerTable;
+
+import java.util.HashSet;
+import java.util.List;
+import java.util.ResourceBundle;
+import java.util.Set;
+
+
+public class ColumnConfigDialog extends Dialog {
+
+    private MyTreeViewerTable treeViewer;
+    private ResourceBundle bundle;
+    private final Set<IColumn> invisibleColumn = new HashSet<>();
+
+    public ColumnConfigDialog(ResourceBundle resourceBundle, MyTreeViewerTable tree, Shell parent) {
+        super(parent);
+        this.treeViewer = tree;
+        this.bundle = resourceBundle;
+        Set<IColumn> columns = this.treeViewer.getInvisibleColumns();
+        if (columns != null) {
+            this.invisibleColumn.addAll(columns);
+        }
+    }
+
+    @Override
+    protected void configureShell(Shell newShell) {
+        super.configureShell(newShell);
+        newShell.setText("Column visibility");
+    }
+
+    @Override
+    protected Control createDialogArea(Composite parent) {
+        Composite container = (Composite) super.createDialogArea(parent);
+
+        GridLayout layout = new GridLayout(2, false);
+        layout.marginRight = 5;
+        layout.marginLeft = 10;
+        layout.marginTop = 10;
+        container.setLayout(layout);
+
+        MyTreeViewerDataSource dataSource = treeViewer.getDataSource();
+
+        List<IColumn> columns = dataSource.getColumns();
+        for (IColumn column : columns) {
+            Button checkBoxButton = new Button(container, SWT.CHECK);
+            checkBoxButton.setText(dataSource.getLocalizeString(column.getColumnName()));
+            checkBoxButton.setSelection(!invisibleColumn.contains(column));
+            checkBoxButton.addListener(SWT.Selection, event -> {
+                if (invisibleColumn.contains(column)) {
+                   invisibleColumn.remove(column);
+                } else {
+                    invisibleColumn.add(column);
+                }
+            });
+        }
+
+        return container;
+    }
+
+    @Override
+    protected void okPressed() {
+        this.treeViewer.setInvisibleColumns(invisibleColumn);
+        super.okPressed();
+    }
+}
---
+++ treeviewer/utils/Images.java
@@ -0,0 +1,42 @@
+package ru.taximaxim.treeviewer.utils;
+
+
+import java.util.ResourceBundle;
+
+public enum Images {
+
+
+    UPDATE,
+    FILTER,
+    TABLE,
+    DEFAULT;
+
+    Images() {
+    }
+
+    public String getImageAddr() {
+        switch(this) {
+            case UPDATE:
+                return "images/update_16.png";
+            case FILTER:
+                return "images/filter.png";
+            case TABLE:
+                return "images/table_16.png";
+            default:
+                return "images/void_16.png";
+        }
+    }
+
+    public String getDescription(ResourceBundle resources) {
+        switch(this) {
+            case UPDATE:
+                return resources.getString("update");
+            case FILTER:
+                return resources.getString("process_filter");
+            case TABLE:
+                return resources.getString("columns");
+            default:
+                return resources.getString("default_action");
+        }
+    }
+}
