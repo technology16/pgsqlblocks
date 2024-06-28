5dd92a5cb4a82b04c2850cce5a6420039f62271a
Shamsutdinov Elmir <84476539+baslo2@users.noreply.github.com>
2024-06-28 10:53:50 +0300

    PGCK 366 updated license (#65)
    updated license

--- src/main/java/ru/taximaxim/pgsqlblocks/dialogs/DBProcessInfoDialog.java
+++ src/main/java/ru/taximaxim/pgsqlblocks/dialogs/DBProcessInfoDialog.java
@@ -1,23 +1,21 @@
-/*-
- * ========================LICENSE_START=================================
- * pgSqlBlocks
- * %
- * Copyright (C) 2017 - 2022 "Technology" LLC
- * %
+/*******************************************************************************
+ * Copyright 2017-2024 TAXTELECOM, LLC
+ *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
  * You may obtain a copy of the License at
- * 
- *      http://www.apache.org/licenses/LICENSE-2.0
- * 
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
  * Unless required by applicable law or agreed to in writing, software
  * distributed under the License is distributed on an "AS IS" BASIS,
  * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
  * See the License for the specific language governing permissions and
  * limitations under the License.
- * =========================LICENSE_END==================================
- */
+ *******************************************************************************/
 package ru.taximaxim.pgsqlblocks.dialogs;
+
+import java.util.ResourceBundle;
 
 import org.eclipse.jface.dialogs.Dialog;
 import org.eclipse.swt.SWT;
@@ -26,12 +24,16 @@ import org.eclipse.swt.events.SelectionEvent;
 import org.eclipse.swt.events.SelectionListener;
 import org.eclipse.swt.layout.GridData;
 import org.eclipse.swt.layout.GridLayout;
-import org.eclipse.swt.widgets.*;
+import org.eclipse.swt.widgets.Button;
+import org.eclipse.swt.widgets.Composite;
+import org.eclipse.swt.widgets.Control;
+import org.eclipse.swt.widgets.Label;
+import org.eclipse.swt.widgets.Shell;
+import org.eclipse.swt.widgets.Text;
+
 import ru.taximaxim.pgsqlblocks.common.models.DBBlocksJournalProcess;
 import ru.taximaxim.pgsqlblocks.common.models.DBProcess;
 import ru.taximaxim.pgsqlblocks.utils.DateUtils;
-
-import java.util.ResourceBundle;
 
 /**
  * Dialog for current process info
--- src/test/java/ru/taximaxim/pgsqlblocks/modules/db/controller/DBControllerTest.java
+++ src/test/java/ru/taximaxim/pgsqlblocks/modules/db/controller/DBControllerTest.java
@@ -1,32 +1,22 @@
-/*
- * ========================LICENSE_START=================================
- * pgSqlBlocks
- * %
- * Copyright (C) 2017 - 2022 "Technology" LLC
- * %
+/*******************************************************************************
+ * Copyright 2017-2024 TAXTELECOM, LLC
+ *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
  * You may obtain a copy of the License at
  *
- *      http://www.apache.org/licenses/LICENSE-2.0
+ *     http://www.apache.org/licenses/LICENSE-2.0
  *
  * Unless required by applicable law or agreed to in writing, software
  * distributed under the License is distributed on an "AS IS" BASIS,
  * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
  * See the License for the specific language governing permissions and
  * limitations under the License.
- * =========================LICENSE_END==================================
- */
+ *******************************************************************************/
 package ru.taximaxim.pgsqlblocks.modules.db.controller;
 
-import com.typesafe.config.Config;
-import com.typesafe.config.ConfigFactory;
-import org.junit.*;
-import ru.taximaxim.pgsqlblocks.common.models.DBModel;
-import ru.taximaxim.pgsqlblocks.common.models.DBProcess;
-import ru.taximaxim.pgsqlblocks.common.models.DBProcessStatus;
-import ru.taximaxim.pgsqlblocks.modules.db.model.DBStatus;
-import ru.taximaxim.pgsqlblocks.utils.Settings;
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertTrue;
 
 import java.io.IOException;
 import java.net.URISyntaxException;
@@ -34,15 +24,33 @@ import java.nio.charset.Charset;
 import java.nio.file.Files;
 import java.nio.file.Path;
 import java.nio.file.Paths;
-import java.sql.*;
-import java.util.*;
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Optional;
 import java.util.concurrent.CompletableFuture;
 import java.util.concurrent.ExecutionException;
 import java.util.stream.Collectors;
 import java.util.stream.Stream;
 
-import static org.junit.Assert.assertEquals;
-import static org.junit.Assert.assertTrue;
+import org.junit.After;
+import org.junit.AfterClass;
+import org.junit.Before;
+import org.junit.BeforeClass;
+import org.junit.Test;
+
+import com.typesafe.config.Config;
+import com.typesafe.config.ConfigFactory;
+
+import ru.taximaxim.pgsqlblocks.common.models.DBModel;
+import ru.taximaxim.pgsqlblocks.common.models.DBProcess;
+import ru.taximaxim.pgsqlblocks.common.models.DBProcessStatus;
+import ru.taximaxim.pgsqlblocks.modules.db.model.DBStatus;
+import ru.taximaxim.pgsqlblocks.utils.Settings;
 
 public class DBControllerTest {
     static class Listener implements DBControllerListener {
